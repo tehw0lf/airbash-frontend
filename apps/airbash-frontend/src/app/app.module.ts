08bb1742ef6c9572c159fa4f436e32f710130c56
tehw0lf <tehwolf@protonmail.com>
2021-10-14 22:05:05 +0200

    Add Angular Material
    To be able to define the data source

--- apps/airbash-frontend/src/app/app.module.ts
+++ apps/airbash-frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
 })

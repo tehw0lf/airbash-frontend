7bfe3e2459397c187f35f4455cdd306e869e3c15
tehw0lf <tehwolf@protonmail.com>
2021-10-16 01:37:36 +0200

    Add HttpClient
    and fix API_URL

--- apps/airbash-frontend/src/app/app.module.ts
+++ apps/airbash-frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,7 +8,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

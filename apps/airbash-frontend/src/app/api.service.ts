039598d55d02b9f5e508a9ab2d61db4ee4c2c317
tehw0lf <tehwolf@protonmail.com>
2021-10-16 17:18:00 +0200

    Handle errors

--- apps/airbash-frontend/src/app/api.service.ts
+++ apps/airbash-frontend/src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { environment } from '../environments/environment';
 import { Capture } from './types/capture';
@@ -12,21 +13,32 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getCaptures(): Observable<Capture[]> {
-    return this.http.get<Capture[]>(`${environment.API_URL}/captures`);
+    return this.http.get<Capture[]>(`${environment.API_URL}/captures`).pipe(
+      catchError((error) => {
+        throw `Failed to get captures: ${error}`;
+      })
+    );
   }
 
   updateCapture(capture: Capture): Observable<void> {
     if (capture.id >= 0) {
-      return this.http.put<void>(
-        `${environment.API_URL}/captures/${capture.id}`,
-        capture
-      );
+      return this.http
+        .put<void>(`${environment.API_URL}/captures/${capture.id}`, capture)
+        .pipe(
+          catchError((error) => {
+            throw `Failed to update capture: ${error}`;
+          })
+        );
     } else {
-      throw new Error('Capture without id cannot be matched');
+      throw new Error('Field capture.id is required to update capture');
     }
   }
 
   deleteCapture(id: number): Observable<void> {
-    return this.http.delete<void>(`${environment.API_URL}/captures/${id}`);
+    return this.http.delete<void>(`${environment.API_URL}/captures/${id}`).pipe(
+      catchError((error) => {
+        throw `Failed to delete capture ${id}: ${error}`;
+      })
+    );
   }
 }

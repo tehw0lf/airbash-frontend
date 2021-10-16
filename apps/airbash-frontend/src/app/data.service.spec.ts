1c97454c7105dafcd991006acccdd2a057dabfd4
tehw0lf <tehwolf@protonmail.com>
2021-10-16 17:36:34 +0200

    WIP: Test Services

--- apps/airbash-frontend/src/app/data.service.spec.ts
+++ apps/airbash-frontend/src/app/data.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
+import { ApiService } from './api.service';
 import { DataService } from './data.service';
+import { mockCaptures } from './testdata/fixtures';
+import { Capture } from './types/capture';
+
+const mockApiService = {
+  getCaptures: jest.fn(() => of(mockCaptures)),
+  updateCapture: jest.fn(() => of()),
+  deleteCapture: jest.fn(() => of()),
+};
 
 describe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: ApiService, useValue: mockApiService }],
+    });
     service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch captures', (done) => {
+    service.captures$
+      .pipe(
+        tap((captures: Capture[]) => {
+          expect(captures).toEqual(mockCaptures);
+          done();
+        })
+      )
+      .subscribe();
   });
 });
---
+++ apps/airbash-frontend/src/app/testdata/fixtures.ts
@@ -0,0 +1,34 @@
+import { Capture } from '../types/capture';
+
+export const mockCaptures: Capture[] = [
+  {
+    id: 0,
+    latitude: '',
+    longitude: '',
+    bssid: '',
+    essid: 'a',
+    pmkid: '',
+    psk: '',
+    processed: 0,
+  },
+  {
+    id: 1,
+    latitude: '',
+    longitude: '',
+    bssid: '',
+    essid: 'b',
+    pmkid: '',
+    psk: '',
+    processed: 0,
+  },
+  {
+    id: 2,
+    latitude: '',
+    longitude: '',
+    bssid: '',
+    essid: 'c',
+    pmkid: '',
+    psk: '',
+    processed: 0,
+  },
+];

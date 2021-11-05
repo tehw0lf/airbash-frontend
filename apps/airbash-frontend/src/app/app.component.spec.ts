b23c79bce386f6e21b4bafdab7e356a61e0618af
tehw0lf <tehwolf@protonmail.com>
2021-11-05 23:29:31 +0100

    Fix app component test

--- apps/airbash-frontend/src/app/app.component.spec.ts
+++ apps/airbash-frontend/src/app/app.component.spec.ts
@@ -2,15 +2,26 @@ import { TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import { of } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
 import { TableEditorComponent } from './table-editor/table-editor.component';
+import { mockCaptures } from './testdata/fixtures';
+import { Capture } from './types/capture';
+
+const mockDataService = {
+  captures$: of<Capture[]>(mockCaptures),
+  editCapture: jest.fn(),
+  removeCapture: jest.fn(),
+};
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent, TableEditorComponent],
       imports: [MatTableModule, MatIconModule, MatInputModule],
+      providers: [{ provide: DataService, useValue: mockDataService }],
     }).compileComponents();
   });
 

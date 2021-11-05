7e1a03c5620c0803ce3d926f94a7f56e68458883
tehw0lf <tehwolf@protonmail.com>
2021-11-05 23:26:13 +0100

    Import modules

--- apps/airbash-frontend/src/app/app.component.spec.ts
+++ apps/airbash-frontend/src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { TableEditorComponent } from './table-editor/table-editor.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, TableEditorComponent],
+      imports: [MatTableModule, MatIconModule, MatInputModule],
     }).compileComponents();
   });
 
